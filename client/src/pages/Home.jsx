406d7d6d113bacda1e971e5ff95526a1db658c1d
adityasharma0131 <adityasharma0431@gmail.com>
2024-07-17 13:03:32 +0530

    we are section completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,7 +7,33 @@ const Home = () => {
       <div className="homepage">
         <img className="hero-logo" src={logo} alt="Logo" />
       </div>
-      <div className="weare"></div>
+      <div className="weare">
+        <div className="bgbox">
+          <h1 className="heading1">We Are,</h1>
+          <h1 className="heading2">M.K Hydraulics</h1>
+
+          <p className="para">
+            M.K Hydraulics is a leading manufacturer renowned for its expertise
+            in delivering high-quality hydraulic solutions. With a diverse
+            product portfolio that includes hydraulic power packs, hydraulic
+            cylinders, pharma machines, marine hydraulic power pack units, and
+            lubrication systems, we cater to a broad range of industrial
+            applications. Our specialized offerings extend to steel mill
+            hydraulic units and hydraulic bag lifting devices for centrifuges,
+            reflecting our commitment to innovation and meeting the unique
+            demands of each sector. Quality is at the heart of everything we do
+            at M.K Hydraulics. Our team of seasoned professionals ensures that
+            every product is meticulously engineered to meet the highest
+            standards of performance, reliability, and durability. We utilize
+            cutting-edge technology and adhere to industry best practices to
+            design and manufacture solutions that not only enhance operational
+            efficiency but also stand the test of time. Our rigorous quality
+            control processes ensure that every product leaving our facility is
+            of the utmost quality, providing our customers with the confidence
+            and peace of mind they deserve.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
