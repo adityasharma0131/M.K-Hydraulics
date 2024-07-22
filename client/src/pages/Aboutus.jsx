5859608acd27eee4ef993c136a042d0d9a476c72
adityasharma0131 <adityasharma0431@gmail.com>
2024-07-23 00:19:56 +0530

    about page bgbox

--- client/src/pages/Aboutus.jsx
+++ client/src/pages/Aboutus.jsx
@@ -8,6 +8,16 @@ const Aboutus = () => {
         <div className="aboutbgbox">
           <h1 className="heading1">Welcome to M.K Hydraulics</h1>
         </div>
+        <div className="about-page">
+          <div className="about-sec">
+            <div className="about-bgbox">
+              <h1>
+                <span>01 -</span>
+                 Who are we
+              </h1>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
