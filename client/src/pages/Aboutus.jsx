964a416fb9e7bbfe9ec2e15896e45b5b39b8dce0
adityasharma0131 <adityasharma0431@gmail.com>
2024-07-22 23:36:26 +0530

    started with aboutUs page

--- client/src/pages/Aboutus.jsx
+++ client/src/pages/Aboutus.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import HeroPage from '../components/HeroPage'
+import React from "react";
+import HeroPage from "../components/HeroPage";
 const Aboutus = () => {
   return (
-    <HeroPage heading="About Us" />
-  )
-}
+    <>
+      <HeroPage heading="About Us" />
+      <div className="about-us">
+        <div className="aboutbgbox">
+          <h1 className="heading1">Welcome to M.K Hydraulics</h1>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Aboutus
+export default Aboutus;
