08d523798a9d6d9d7e7a63d2574e2f4f66f69589
adityasharma0131 <adityasharma0431@gmail.com>
2024-07-29 18:49:42 +0530

    footer image edit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/Black_and_Blue_Airplane_Travel_Logo-removebg-preview.png";
+import logo from "../assets/footer-logo.png";
 import { Link } from "react-router-dom";
 import { IoIosArrowForward } from "react-icons/io";
 import { FaLocationDot } from "react-icons/fa6";
--- client/src/pages/AdminLogin.jsx
+++ client/src/pages/AdminLogin.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import HeroPage from '../components/HeroPage'
 
 const AdminLogin = () => {
   return (
-    <div>AdminLogin</div>
+    <>
+    <div className='loginbgbox'>
+      <h1 className='heading'>ADmin Login</h1>
+
+    </div>
+    </>
   )
 }
 
