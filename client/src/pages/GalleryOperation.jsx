6220fd6740c9f9ae71ed9fc2207c03e932cf6646
adityasharma0131 <adityasharma0431@gmail.com>
2024-08-01 18:12:22 +0530

    Gallery Page completed

--- client/src/pages/GalleryOperation.jsx
+++ client/src/pages/GalleryOperation.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import { MdEditNote } from "react-icons/md";
+import { AiFillDelete } from "react-icons/ai";
 
 const GalleryOperation = () => {
-  return (
-    <div>GalleryOperation</div>
-  )
-}
+  const gallery = [
+    {
+      id: 1,
+      image: "https://picsum.photos/seed/picsum/500/500",
+      filename: "Category 1",
+    },
+  ];
 
-export default GalleryOperation
+  return (
+    <>
+      <div className="dashboard-name">
+        <h1>Gallery Page</h1>
+      </div>
+
+      <div className="tables-area">
+        <div className="product-listing">
+          <div className="operation-header">
+            <h1 className="heading">Products</h1>
+            <button className="add">Add Product +</button>
+          </div>
+          <table className="modern-table">
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>File Name</th>
+                <th>Operation</th>
+              </tr>
+            </thead>
+            <tbody>
+              {gallery.map((product) => (
+                <tr key={product.id}>
+                  <td>
+                    <img
+                      src={product.image}
+                      alt={`Image of ${product.filename}`}
+                      className="product-image"
+                    />
+                  </td>
+                  <td>{product.filename}</td>
+                  <td>
+                    <AiFillDelete />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default GalleryOperation;
