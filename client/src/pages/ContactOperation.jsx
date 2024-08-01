1a8362c8af88401e52cb3757350c51d118d0e844
adityasharma0131 <adityasharma0431@gmail.com>
2024-08-02 00:05:21 +0530

    ContactOperation completed

--- client/src/pages/ContactOperation.jsx
+++ client/src/pages/ContactOperation.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import { MdEditNote } from "react-icons/md";
+import { AiFillDelete } from "react-icons/ai";
 
 const ContactOperation = () => {
+  const adminUsers = [
+    {
+      id: 1,
+      name: "John Doe",
+      email: "john.doe@example.com",
+      organization: "Organization Name",
+      message:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam dicta error ducimus deleniti modi sunt hic nostrum voluptatum consectetur, dolor ea facere. Architecto laborum corporis delectus unde exercitationem modi quo!",
+    },
+    {
+      id: 1,
+      name: "John Doe",
+      email: "john.doe@example.com",
+      organization: "Organization Name",
+      message:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam dicta error ducimus deleniti modi sunt hic nostrum voluptatum consectetur, dolor ea facere. Architecto laborum corporis delectus unde exercitationem modi quo!",
+    },
+    {
+      id: 1,
+      name: "John Doe",
+      email: "john.doe@example.com",
+      organization: "Organization Name",
+      message:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam dicta error ducimus deleniti modi sunt hic nostrum voluptatum consectetur, dolor ea facere. Architecto laborum corporis delectus unde exercitationem modi quo!",
+    },
+  ];
   return (
-    <div>ContactOperation</div>
-  )
-}
+    <>
+      <div className="dashboard-name">
+        <h1>Contact us Page</h1>
+      </div>
+      <div className="admin-users">
+        <h1 className="heading">Queries</h1>
+        <table className="modern-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Organization</th>
+              <th>Message</th>
+              <th>Operation</th>
+            </tr>
+          </thead>
+          <tbody>
+            {adminUsers.map((user) => (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.organization}</td>
+                <td>{user.message}</td>
+                <td>
+                  {" "}
+                  <AiFillDelete />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
-export default ContactOperation
+export default ContactOperation;
