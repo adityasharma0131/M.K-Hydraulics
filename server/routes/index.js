0a2f566a6d9b63ce753e151159ca712e93d2204c
adityasharma0131 <adityasharma0431@gmail.com>
2024-08-14 20:32:16 +0530

    testing23

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,11 +7,9 @@ const Contact = require("../Models/Contact");
 const UserModel = require("../Models/User");
 const Social = require("../Models/Social");
 const Product = require("../Models/Product");
-const path = require("path");
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, "public")));
-app.get("/", async (req, res) => {
+
+router.get("/", async (req, res) => {
   try {
     res.render("index", { title: "My Express App" });
   } catch (error) {
@@ -19,6 +17,7 @@ app.get("/", async (req, res) => {
     res.status(500).send("An error occurred");
   }
 });
+
 
 // Add a new category
 router.post("/add-category", async (req, res) => {
